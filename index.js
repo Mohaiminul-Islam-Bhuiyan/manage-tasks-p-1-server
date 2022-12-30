c87c998afc5111a2070fb3911bf4b6c98fbd157e
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-12-30 20:02:46 +0600

    task add and view added

--- index.js
+++ index.js
@@ -18,6 +18,43 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const taskCollection = client.db('manageTask').collection('alltask')
+
+        app.post('/task', async (req, res) => {
+            const task = req.body
+            const result = await taskCollection.insertOne(task)
+            res.send(result)
+        })
+
+        app.get('/alltask', async (req, res) => {
+            let query = {}
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = taskCollection.find(query)
+            const tasks = await cursor.toArray()
+            res.send(tasks)
+        })
+
+
+
+    }
+    finally {
+
+    }
+}
+
+run()
+    .catch(err => console.log(err))
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('server is running')
 })
